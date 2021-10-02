27e1a8effbdeeeadd93dabf7c78871e00a547a57
Asher <asher.bergtraun@gmail.com>
2021-10-01 19:54:17 -0700

    completed reducer.js

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -27,32 +27,32 @@ const reducer = (state = initialState, action) => {
     case UPDATE_PRODUCTS:
       return {
         ...state,
-        products: [...action.products],
+        products: [...action.products]
       };
     //if action.type equals UPDATE_CATEGORIESS -> return new state object with new products array
     case UPDATE_CATEGORIES:
       return {
         ...state,
-        categories: [...action.categories],
+        categories: [...action.categories]
       };
 
     case UPDATE_CURRENT_CATEGORY:
       return {
         ...state,
         currentCategory: action.currentCategory
-      }
+      };
 
     case ADD_TO_CART:
       return {
         ...state,
         cartOpen: true,
-        cart: [...state.cart, action.product],
+        cart: [...state.cart, action.product]
       };
 
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
-        cart: [...state.cart, ...action.products],
+        cart: [...state.cart, ...action.products]
       };
     
     case REMOVE_FROM_CART:
@@ -73,7 +73,7 @@ const reducer = (state = initialState, action) => {
           if (action._id === product._id) {
             product.purchaseQuantity = action.purchaseQuantity
           }
-          return product
+          return product;
         })
       };
 
